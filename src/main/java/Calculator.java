8541ec5d0125973bfbfd4bfdc9c9d302eed9f940
RAID-OTHMANE <othmane.raaiid@gmail.com>
2024-12-14 19:52:35 +0100

    Ajout des méthodes @BeforeEach et @AfterEach pour simplifier les tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,13 @@
 public class Calculator {
+    public int add(int opG, int opD) {
+        return opG + opD;
+    }
+
+    public int divide(int opG, int opD) {
+        if (opD == 0) {
+            throw new IllegalArgumentException("Division par zéro non autorisée");
+        }
+        return opG / opD;
+    }
+
 }
